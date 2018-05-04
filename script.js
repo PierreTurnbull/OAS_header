8a2601641bf3a36e2b379baa9c29f8c4a3bb86d1
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-05-04 21:34:20 +0200

    Mobile height dimensions fix.

--- script.js
+++ script.js
@@ -4,22 +4,40 @@ var menu      = document.querySelector(".desktop-menu");
 var shareBtn  = document.querySelector(".share-btn");
 var body      = document.body;
 
-function openMenu() {
+function openMenuS() {
+    console.log("open S");
     menu.style.left = "0";
     shareBtn.style.left = "0";
     body.style.overflow = "hidden";
 }
 
-function closeMenu() {
+function openMenuM() {
+    console.log("open M");
+}
+
+function closeMenuS() {
+    console.log("close S");
     menu.style.left = "-100vw";
     shareBtn.style.left = "-200vw";
     body.style.overflow = "scroll";
 }
 
+function closeMenuM() {
+    console.log("close M");
+}
+
 navOpener.addEventListener("click", () => {
-    openMenu();
+    if (window.innerWidth >= 1024) {
+        openMenuM();
+    } else {
+        openMenuS();
+    }
 });
 
 navCloser.addEventListener("click", () => {
-    closeMenu();
+    if (window.innerWidth >= 1024) {
+        closeMenuM();
+    } else {
+        closeMenuS();
+    }
 });
