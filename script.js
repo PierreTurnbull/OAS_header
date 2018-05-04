84a38ab218ddcc204bf8f70a9862c6a99133479a
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-05-04 16:23:00 +0200

    Mobile header done.

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var navOpener = document.querySelector(".open-nav");
 var navCloser = document.querySelector(".close-nav");
 var menu      = document.querySelector(".desktop-menu");
+var shareBtn  = document.querySelector(".share-btn");
+var body      = document.body;
 
 function openMenu() {
     menu.style.left = "0";
+    shareBtn.style.left = "0";
+    body.style.overflow = "hidden";
 }
 
 function closeMenu() {
     menu.style.left = "-100vw";
+    shareBtn.style.left = "-200vw";
+    body.style.overflow = "scroll";
 }
 
 navOpener.addEventListener("click", () => {
