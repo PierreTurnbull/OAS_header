326246cc1072da157ef1beeb5f029c1c487c5b14
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-05-04 23:10:16 +0200

    Tablet done.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var navOpener = document.querySelector(".open-nav");
-var navCloser = document.querySelector(".close-nav");
-var menu      = document.querySelector(".desktop-menu");
-var shareBtn  = document.querySelector(".share-btn");
-var body      = document.body;
+var navOpener       = document.querySelector(".open-nav");
+var navCloser       = document.querySelector(".close-nav");
+var navOpenerImg    = document.querySelector(".open-nav-img");
+var menu            = document.querySelector(".desktop-menu");
+var shareBtn        = document.querySelector(".share-btn");
+var navList         = document.querySelector(".nav-list-menu");
+var body            = document.body;
+
+var menuIsOpen      = false;
 
 function openMenuS() {
     console.log("open S");
@@ -13,6 +17,13 @@ function openMenuS() {
 
 function openMenuM() {
     console.log("open M");
+    navOpenerImg.src = "img/close-nav.png";
+    navOpenerImg.style.height = "22px";
+    navList.style.display = "block";
+    setTimeout(() => {
+        navList.style.height = "500px";
+    }, 0);
+    menuIsOpen = true;
 }
 
 function closeMenuS() {
@@ -24,20 +35,25 @@ function closeMenuS() {
 
 function closeMenuM() {
     console.log("close M");
+    navOpenerImg.src = "img/open-nav.png";
+    navOpenerImg.style.height = "15px";
+    navList.style.height = "0px";
+    setTimeout(() => {
+        navList.style.display = "none";
+    }, 300);
+    menuIsOpen = false;
 }
 
 navOpener.addEventListener("click", () => {
-    if (window.innerWidth >= 1024) {
+    if (window.innerWidth >= 1024 && !menuIsOpen) {
         openMenuM();
+    } else if (window.innerWidth >= 1024) {
+        closeMenuM();
     } else {
         openMenuS();
     }
 });
 
 navCloser.addEventListener("click", () => {
-    if (window.innerWidth >= 1024) {
-        closeMenuM();
-    } else {
-        closeMenuS();
-    }
+    closeMenuS();
 });
